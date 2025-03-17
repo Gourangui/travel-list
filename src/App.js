4ae131b1e364becafbe4904d443afb6d9917897d
Gouri <gouranguidevi@gmail.com>
2025-03-17 17:39:29 -0300

    WIP: remover item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './index.css';
+import { useState } from 'react';
 
-const initialItems = [
+/* const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 5, packed: true },
-];
+]; */
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  function handleAddItems(newItem){
+    setItems((items) => [...items, newItem]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackagingList />
+      <Form onAddItems={handleAddItems} />
+      <PackagingList items={items} />
       <Stats />
     </div>
   );
@@ -25,19 +31,51 @@ function Logo(){
   );
 }
 
-function Form(){
+/* add item */
+function Form({ onAddItems }){
+  const [description, setDescription] = React.useState('');
+  const [quantity, setQuantity] = React.useState(1);
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Math.random(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    onAddItems(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map 
+        ((num) => (
+          <option value={num} key={num}>{num}</option>
+        ))}
+      </select>
+      <input type='text' placeholder='Item...' value={description} onChange={(e) => {
+        setDescription(e.target.value);
+      }}/>
+    </form>
   )
 }
 
-function PackagingList(){
+/* list of items */
+function PackagingList({items}){
   return (
     <div className='list'>
       <ul>
-        {initialItems.map(item => (
+        {items.map(item => (
           <Item key={item.id} item={item} />
         ))}
       </ul>
@@ -48,7 +86,10 @@ function PackagingList(){
 function Item({ item }){
   return (
     <li>
-      <input type='checkbox' checked={item.packed} />
+      <input 
+      type='checkbox' 
+      checked={item.packed} 
+      onClick={item.packed ? {textDecoration: "line-through"} : {}}/>
       <span>{item.quantity} {item.description}</span>
       <button>❌</button>
     </li>
